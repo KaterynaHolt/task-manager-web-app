040f0a391ce95968442b5553f1c374d34d423e6d
KaterynaHolt <kholtvianskatech@gmail.com>
2023-11-27 05:47:35 +0200

    TMWA-14-Connect-React-and-Spring

--- src/main/java/com/taskmanager/app/taskmanagerwebapp/controller/TaskController.java
+++ src/main/java/com/taskmanager/app/taskmanagerwebapp/controller/TaskController.java
@@ -3,12 +3,15 @@ package com.taskmanager.app.taskmanagerwebapp.controller;
 import com.taskmanager.app.taskmanagerwebapp.model.Task;
 import com.taskmanager.app.taskmanagerwebapp.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/taskmanager/")
 public class TaskController {
@@ -19,5 +22,10 @@ public class TaskController {
     @GetMapping("/tasks")
     public List<Task> getAllTasks(){
         return taskRepository.findAll();
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, the time at the server is now " + new Date() + "\n";
     }
 }
